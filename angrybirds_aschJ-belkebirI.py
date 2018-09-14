a36bd4a48563e78a7acc850a9d0247100e211d05
Imad <ibelkebir@stuy.edu>
2018-09-13 20:49:20 -0400

    Created random occupation func

---
+++ angrybirds_aschJ-belkebirI.py
@@ -0,0 +1,39 @@
+# Team Angry Birds - Jared Asch, Imad Belkebir
+# SoftDev1 pd7
+# K#06 -- StI/O: Divine your Destiny!
+# 2018-09-13
+
+from random import random
+
+def readlines():
+    f = open('occupations.csv', 'r')
+    text = f.read().strip().split('\n')
+    f.close()
+    return text[1:][:-1]
+
+def linesToDict(lines):
+    info_dict = {}
+    for line in lines:
+        if line[0] is '\"':
+            end = line[1:].find('\"')
+            job = line[1:end+1]
+            info_dict[job] = float(line[end+3:])
+        else:
+            job = line.split(',')[0]
+            info_dict[job] = float(line.split(',')[1])
+    return info_dict
+
+def pickOcc(Occupations):
+    percents = list(Occupations.values())
+    occs = list(Occupations.keys())
+    rand = random() * 99.8
+    percentTot = percents[0];
+    index = 0;
+    while(percentTot < rand):
+        index += 1
+        percentTot += percents[index]
+    return (occs[index])
+
+
+print(pickOcc(linesToDict(readlines())));
+
