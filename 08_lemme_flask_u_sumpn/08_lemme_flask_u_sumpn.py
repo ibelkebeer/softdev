013304b890d3727539499328b449256d3deb0cd2
Imad <ibelkebir@stuy.edu>
2018-09-19 14:24:57 -0400

    cleanup

---
+++ 08_lemme_flask_u_sumpn/08_lemme_flask_u_sumpn.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def page1():
+    return '<a href=/page2>page 2</a> <br> <a href=/page3>page 3</a>'
+
+@app.route('/page2')
+def page2():
+    return '<a href=/>page 1</a> <br> <a href=/page3>page 3</a>'
+
+@app.route('/page3')
+def page3():
+    return '<a href=/>page 1</a> <br> <a href=/page2>page 2</a>'
+
+app.debug = True
+app.run()
