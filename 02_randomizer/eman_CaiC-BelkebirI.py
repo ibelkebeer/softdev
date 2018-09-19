04c93c686f2ad2aee213330bf2910b2b134d24d2
Imad <ibelkebir@stuy.edu>
2018-09-19 14:27:50 -0400

    fixing up

---
+++ 02_randomizer/eman_CaiC-BelkebirI.py
@@ -0,0 +1,37 @@
+#Team Eman - Cathy Cai and Imad Belkebirl
+#SoftDev1 pd07
+#K2 -- NO-body expects the Spanish Inquisition!
+#2018-09-08
+
+import random
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek',
+              'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara',
+              'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']}
+
+
+def printRandomStudent():
+
+    #the user chooses a random key
+    team = input("Team w, m, or x?: ")
+
+    if team in KREWES.keys():
+
+        #choose a random name from the chosen key's value
+        student = random.choice(KREWES[team])
+
+        #print the chosen name
+        print (student)
+
+    else:
+
+        #the user does not choose w, m, or x
+        print ("Choose either w, m, or x.")
+
+printRandomStudent()
