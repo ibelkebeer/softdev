82c7bee2c5e0a763da138ddbc0578a864d9b6aa2
Imad <ibelkebir@stuy.edu>
2018-09-29 12:37:20 -0400

    updated to use post

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,6 +1,6 @@
 #Imad Belkebir
 #SoftDev1 pd7
-#K12 -- 
+#K12 --
 #2018-09/27
 
 from flask import Flask, render_template, request
@@ -9,16 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return render_template("temp.html")
-    
-@app.route("/auth")
-def form():
-    print(request.method[:4])
-    return 'The username entered was <font color="red">' + \
-      request.args['username'] + '</font><br>The request method used was <font color="blue">' + \
-      request.method[:4] + '</font><br><font color="purple">Thanks'
 
+@app.route("/auth", methods=['GET','POST'])
+def form():
+    if(request.method == 'GET'):
+        return 'The username entered was <font color="red">' + \
+          request.args['username'] + '</font><br>The request method used was <font color="blue">' + \
+          request.method + '</font><br><font color="purple">Thanks'
+    else:
+        return 'The username entered was <font color="red">' + \
+          request.form['username'] + '</font><br>The request method used was <font color="blue">' + \
+          request.method + '</font><br><font color="purple">Thanks'
 if __name__ == "__main__":
     app.debug = True
     app.run();
-    
-    
