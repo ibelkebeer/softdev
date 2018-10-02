add4b38f01dcc1f3c8d81b4f563c88c1417a332b
Imad <ibelkebir@stuy.edu>
2018-10-01 23:39:43 -0400

    Now with comments

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,7 +1,7 @@
-#Imad Belkebir
-#SoftDev1 pd7
-#K14 -- Do I Know You?
-#2018-10-01
+# Flasking your name -- Imad Belkebir, Theodore Peters
+# SoftDev1 pd7
+# K14 -- Do I Know You?
+# 2018-10-02
 
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
@@ -14,19 +14,19 @@ app.secret_key = os.urandom(32)
 @app.route("/")
 def root():
     print(session)
-    if 'username' in session.keys():
+    if 'username' in session and session['username'] == user: # if user already logged in
         return render_template("welcome.html", var1=session['username'])
-    return redirect(url_for("login"))
+    return redirect(url_for("login")) # unless logged in default redirects to login screen
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-    if request.method == "GET":
+    if request.method == "GET": # default redirect from route
         return render_template("login.html")
-    if request.form.get('username') != user:
+    if request.form.get('username') != user: # if username is invalid
         return render_template("login.html", var1="Error! User does not exist")
-    if request.form.get('pass') != password:
+    if request.form.get('pass') != password: # if password is invalid
         return render_template("login.html", var1="Error! Incorect password")
-    session['username'] = request.form.get('username')
+    session['username'] = request.form.get('username') # correct login
     return redirect(url_for("root"))
 
 @app.route("/logout")
