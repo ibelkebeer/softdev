2b88be8dc14d33823bcb949cbc8cd2414fb9ddea
Imad <ibelkebir@stuy.edu>
2018-10-05 20:58:45 -0400

    able to create table with averages

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -10,35 +10,40 @@ DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
+db.text_factory = str         #change data from fethall() from unicode to utf
 
 #=======================================================================
 
 command = "DROP TABLE peeps_avg"
 c.execute(command)
-command = "CREATE TABLE peeps_avg (name TEXT, id INTEGER PRIMARY KEY, average INTEGER)"
+command = "CREATE TABLE peeps_avg (name TEXT, id INTEGER, average INTEGER)"
 c.execute(command)
 
 data = []
-individual_data = []
 
-command = "SELECT peeps.name, peeps.id FROM peeps, courses WHERE peeps.id = courses.id"
+command = "SELECT peeps.name, peeps.id FROM peeps"
 c.execute(command)
 
 for peep in c.fetchall():
-    command = "SELECT courses.mark FROM courses WHERE courses.id = "+str(peep[1])
+    individual_data = []
+    command = "SELECT courses.mark FROM courses WHERE courses.id = " + str(peep[1])
     c.execute(command)
     sum = 0
     num = 0
     for mark in c.fetchall():
         sum += mark[0]
         num += 1
-    print(sum)
-    print(num)
     sum /= num
-    peep[2] = sum
-    
-c.executemany("INSERT INTO peeps_avg VALUES (?, ?, ?)", (c.fetchall()))
+    individual_data.append(peep[0])
+    individual_data.append(peep[1])
+    individual_data.append(sum)
+    data.append(individual_data)
 
+c.executemany("INSERT INTO peeps_avg VALUES (?, ?, ?)", data)
+
+command = "SELECT * FROM peeps_avg"
+c.execute(command)
+print(c.fetchall())
 
 db.commit()
 db.close()
