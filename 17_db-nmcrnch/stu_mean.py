962251ef0804497279cd1854aedd2e2f159e20b5
Imad <ibelkebir@stuy.edu>
2018-10-08 22:51:10 -0400

    Able to add to courses

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -1,10 +1,9 @@
-#
-#
-#
-#
+#Hmmmm - Imad Belkebir
+#SoftDev1 pd7
+#K17 -- Average
+#2018-10-05
 
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
 
 DB_FILE="discobandit.db"
 
@@ -14,36 +13,34 @@ db.text_factory = str         #change data from fethall() from unicode to utf
 
 #=======================================================================
 
-command = "DROP TABLE peeps_avg"
-c.execute(command)
 command = "CREATE TABLE peeps_avg (name TEXT, id INTEGER, average INTEGER)"
 c.execute(command)
 
 data = []
 
-command = "SELECT peeps.name, peeps.id FROM peeps"
+command = "SELECT peeps.name, peeps.id FROM peeps" #retrieves name and id of all peeps
 c.execute(command)
 
-for peep in c.fetchall():
+for peep in c.fetchall(): #cycles through retrieved peeps
     individual_data = []
-    command = "SELECT courses.mark FROM courses WHERE courses.id = " + str(peep[1])
+    command = "SELECT courses.mark FROM courses WHERE courses.id = " + str(peep[1]) #get marks with corresponding id of current peep
     c.execute(command)
     sum = 0
     num = 0
-    for mark in c.fetchall():
+    for mark in c.fetchall(): #cycle through retrieved marks to calculate average
         sum += mark[0]
         num += 1
     sum /= num
     individual_data.append(peep[0])
     individual_data.append(peep[1])
-    individual_data.append(sum)
-    data.append(individual_data)
+    individual_data.append(sum)     #create row for current peep
+    data.append(individual_data)    #insert row into data
 
-c.executemany("INSERT INTO peeps_avg VALUES (?, ?, ?)", data)
+c.executemany("INSERT INTO peeps_avg VALUES (?, ?, ?)", data) #insert all individual into table peeps_avg
 
-command = "SELECT * FROM peeps_avg"
-c.execute(command)
-print(c.fetchall())
+def insertRow(code, mark, id):
+    row = [code, mark, id]
+    c.execute("INSERT INTO courses VALUES (?, ?, ?)", row)
 
 db.commit()
 db.close()
