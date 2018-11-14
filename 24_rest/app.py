de7a7dada44d8a70f49aa6698bc361c9db921da6
Imad <ibelkebir@stuy.edu>
2018-11-13 20:46:50 -0500

    added video

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,13 +1,14 @@
 
 from flask import Flask, render_template
-import urllib.request, json
+import urllib.request
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     url = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=5IRdvy3xah6vato5f9aGiOlqv5WXDpP2khv2nItV")
     data = json.loads(url.read())
-    return render_template("temp.html", data["url"])
+    return render_template("temp.html", pic=data['url'])
 
 if __name__ == "__main__":
     app.debug = True
