a29c7afe3c5fb480a918c44a0a035f715b6d7603
Imad <ibelkebir@stuy.edu>
2018-11-15 20:58:53 -0500

    3  APIs

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -8,14 +8,27 @@ app = Flask(__name__)
 def root():
     url = urllib.request.urlopen("https://ghibliapi.herokuapp.com/films?limit=1")
     data = json.loads(url.read())
+
+    #print(data)
+
     title = data[0]['title']
 
-    url = urllib.request.urlopen("https://api.jikan.moe/v3/search/anime?q=CastleintheSky")
+    url = urllib.request.urlopen("https://api.jikan.moe/v3/search/anime?q=Kob")
     data = json.loads(url.read())
-    img = data['image_url']
-    
+
+    #print(data)
+
+    img = data['results'][1]['image_url']
+
+    url = urllib.request.urlopen("https://dog.ceo/api/breeds/image/random")
+    data = json.loads(url.read())
+
+    #print(data)
+
     return render_template("temp.html",
-    title=title)
+    title=title,
+    pic1=img,
+    pic2=data['message'])
 
 if __name__ == "__main__":
     app.debug = True
