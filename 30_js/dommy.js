6b406262149903861333b38f8c80e73e3d7928c1
Imad <ibelkebir@stuy.edu>
2018-12-20 13:41:59 -0500

    huh

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -8,14 +8,24 @@ var fibonacci = (n) => {
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
+var num = 8;
 var list = document.getElementById("thelist");
+
 list.addEventListener('mouseover', function(e) {
   console.log(e.fromElement.firstChild.nodeValue)
   document.getElementById("h").innerHTML = e.fromElement.firstChild.nodeValue;
-})
+});
 list.addEventListener('mouseout', function(e) {
   document.getElementById("h").innerHTML = "Hello World!";
-})
+});
 list.addEventListener('click', function(e) {
   console.log(e);
-})
+  list.removeChild(e.srcElement);
+});
+
+document.getElementById("b").addEventListener('click', function(){
+    var li = document.createElement("li");
+    li.innerHTML="item " + num;
+    list.appendChild(li);
+    num++;
+});
