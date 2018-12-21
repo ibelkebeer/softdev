09435eb33fc8482c78786d63e8f1ace50bd65dc0
Imad <ibelkebir@stuy.edu>
2018-12-21 13:10:01 -0500

    New List

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -8,8 +8,16 @@ var fibonacci = (n) => {
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
+var fact = (n) => {
+  if ( n == 0 ) {
+    return 1;
+  }
+  return n * fact(n-1);
+}
+
 var listnum = 8;
 var fibnum = 0;
+var factnum = 0;
 var list = document.getElementById("thelist");
 var fibList = document.getElementById("fiblist");
 
@@ -37,4 +45,20 @@ document.getElementById("fb").addEventListener('click', function(){
     li.innerHTML="Fib of " + fibnum + " is " + fibonacci(fibnum);
     fibList.appendChild(li);
     fibnum++;
+});
+
+var button1 = document.getElementsByTagName("button")[0];
+var button2 = document.getElementsByTagName("button")[1];
+var newList = document.createElement("ol");
+var newButton = document.createElement("button");
+newButton.innerHTML = "Fact button";
+newButton.id = "nb";
+button1.parentNode.insertBefore(newList, button1);
+button2.parentNode.insertBefore(newButton, button2.nextSibling);
+
+document.getElementById("nb").addEventListener('click', function(){
+    var li = document.createElement("li");
+    li.innerHTML="Fact of " + factnum + " is " + fact(factnum);
+    newList.appendChild(li);
+    factnum++;
 });
